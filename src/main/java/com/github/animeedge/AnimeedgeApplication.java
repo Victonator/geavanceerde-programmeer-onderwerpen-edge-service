ae8a898b6f189b6d109d742881c3383b19a47fc2
wispae <wilferspaepen@gmail.com>
2022-01-02 15:23:28 +0100

    project creation

---
+++ src/main/java/com/github/animeedge/AnimeedgeApplication.java
@@ -0,0 +1,13 @@
+package com.github.animeedge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeedgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeedgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/animeedge/AnimeedgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.animeedge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeedgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
